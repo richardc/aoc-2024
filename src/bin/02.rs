5548cf7bd3afd521491b60bd73ca81147e40b33a
Richard Clamp <richardc@unixbeard.net>
2024-12-11 16:13:13 +0000

    Day 2 Part 2: Brute force
    Just generate all the 'dampened' versions and check those again.

--- src/bin/02.rs
+++ src/bin/02.rs
@@ -35,8 +35,28 @@ pub fn part_one(input: &str) -> Option<usize> {
     Some(records.filter(|r| is_safe(r)).count())
 }
 
-pub fn part_two(_input: &str) -> Option<u32> {
-    None
+fn is_safe_dampened(record: &[u32]) -> bool {
+    if is_safe(record) {
+        return true;
+    }
+    for i in 0..record.len() {
+        let start = &record[0..i];
+        let end = &record[i + 1..];
+        let test = [start, end].concat();
+        if is_safe(&test) {
+            return true;
+        }
+    }
+    false
+}
+
+pub fn part_two(input: &str) -> Option<usize> {
+    let records = input.lines().map(|l| {
+        l.split_whitespace()
+            .map(|w| w.parse::<u32>().unwrap())
+            .collect::<Vec<_>>()
+    });
+    Some(records.filter(|r| is_safe_dampened(r)).count())
 }
 
 #[cfg(test)]
@@ -52,6 +72,6 @@ mod tests {
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(4));
     }
 }
