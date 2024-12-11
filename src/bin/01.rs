aca0e4b89de5abed50761cff2434cd4dc43c6ceb
Richard Clamp <richardc@unixbeard.net>
2024-12-11 13:28:09 +0000

    Day 1 Part 2: itertools.count() and some sum()

--- src/bin/01.rs
+++ src/bin/01.rs
@@ -14,8 +14,20 @@ pub fn part_one(input: &str) -> Option<u32> {
     Some(left.zip(right).map(|(l, r)| l.abs_diff(r)).sum())
 }
 
-pub fn part_two(_input: &str) -> Option<u32> {
-    None
+pub fn part_two(input: &str) -> Option<u32> {
+    let pairs: Vec<(u32, u32)> = input
+        .lines()
+        .map(|l| {
+            let (left, right) = l.split_once("   ").unwrap();
+            (left.parse().unwrap(), right.parse().unwrap())
+        })
+        .collect();
+    let left = pairs.clone().into_iter().map(|(l, _)| l);
+    let right = pairs.into_iter().map(|(_, r)| r).counts();
+    Some(
+        left.map(|l| right.get(&l).map_or(0, |r| l * *r as u32))
+            .sum(),
+    )
 }
 
 #[cfg(test)]
@@ -31,6 +43,6 @@ mod tests {
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(31));
     }
 }
