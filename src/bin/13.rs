e9e3c59179ea4a439d43ee189c95d43f25bad0ea
Richard Clamp <richardc@unixbeard.net>
2026-05-11 16:30:12 +0100

    Day 13 Part 2: Move the targets
    Thankfully, because we were already doing the maths we just needed to
    add some code to move the goalposts, and to switch from i32 to i64 to
    give the necessary range to the variables.

--- src/bin/13.rs
+++ src/bin/13.rs
@@ -2,9 +2,9 @@ advent_of_code::solution!(13);
 
 #[derive(Debug, Default)]
 struct Machine {
-    a: (i32, i32),
-    b: (i32, i32),
-    prize: (i32, i32),
+    a: (i64, i64),
+    b: (i64, i64),
+    prize: (i64, i64),
 }
 
 impl Machine {
@@ -42,7 +42,7 @@ impl Machine {
         current
     }
 
-    fn fewest_tokens(&self) -> Option<i32> {
+    fn fewest_tokens(&self) -> Option<i64> {
         let b = (self.a.0 * self.prize.1 - self.a.1 * self.prize.0)
             / (self.a.0 * self.b.1 - self.a.1 * self.b.0);
         let a = (self.prize.0 - b * self.b.0) / self.a.0;
@@ -68,18 +68,27 @@ impl Puzzle {
         Self { machines }
     }
 
-    fn fewest_tokens(&self) -> i32 {
+    fn move_targets(&mut self, distance: i64) {
+        for machine in self.machines.iter_mut() {
+            machine.prize.0 += distance;
+            machine.prize.1 += distance;
+        }
+    }
+
+    fn fewest_tokens(&self) -> i64 {
         self.machines.iter().filter_map(|m| m.fewest_tokens()).sum()
     }
 }
 
-pub fn part_one(input: &str) -> Option<i32> {
+pub fn part_one(input: &str) -> Option<i64> {
     let puzzle = Puzzle::from_str(input);
     Some(puzzle.fewest_tokens())
 }
 
-pub fn part_two(_input: &str) -> Option<u32> {
-    None
+pub fn part_two(input: &str) -> Option<i64> {
+    let mut puzzle = Puzzle::from_str(input);
+    puzzle.move_targets(10_000_000_000_000);
+    Some(puzzle.fewest_tokens())
 }
 
 #[cfg(test)]
@@ -95,6 +104,6 @@ mod tests {
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(875318608908));
     }
 }
