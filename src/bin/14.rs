e64e2bdd46a8401f7c26b485663b96eee39d536e
Richard Clamp <richardc@unixbeard.net>
2026-05-12 07:32:24 +0100

    remove bad test

--- src/bin/14.rs
+++ src/bin/14.rs
@@ -169,10 +169,4 @@ mod tests {
         let result = part_one(&advent_of_code::template::read_file("examples", DAY));
         assert_eq!(result, Some(12));
     }
-
-    #[test]
-    fn test_part_two() {
-        let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
-    }
 }
